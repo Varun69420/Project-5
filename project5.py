408a0db057411bb58934cfc5e826c93fadaba9a4
Varun69420 <big.oofers31@gmail.com>
2024-10-04 19:21:11 +0530

    first commit

---
+++ project5.py
@@ -0,0 +1,5 @@
+Clothtype =  int(input("What's your temperature in your area? : "))
+if (Clothtype>30):
+    print("Wear cotton and loose clothes")
+else:
+    print("Wear woolen clothings and wear 2 layered clothings")
